7a86ceced73c554ab852d1d59652339f5d8b737e
jeongik-jo <67620234+jeongik-jo@users.noreply.github.com>
2022-09-06 10:32:45 -0400

    update

--- Dataset.py
+++ Dataset.py
@@ -11,37 +11,51 @@ is_m2 = True
 is_m3 = False
 is_m4 = False
 
+normalize_m = True
+
 
 if is_m1:
-    def m(X):
+    def _m(X):
         return np.log((0.2 * X[:, 0] + 0.9 * X[:, 1]) ** 2) + \
                np.cos(np.pi / np.log((0.5 * X[:, 0] + 0.3 * X[:, 1]) ** 2)) +\
                np.exp(1/50 * (0.7 * X[:, 0] + 0.7 * X[:, 1])) +\
                np.tan(np.pi * (0.1 * X[:, 0] + 0.3 * X[:, 1]) ** 4) / (0.1 * X[:, 0] + 0.3 * X[:, 1]) ** 2
     input_dim = 2
     noise_scale = 5.04
+    m_std = 2.02
 elif is_m2:
-    def m(X):
+    def _m(X):
         return np.tan(np.sin(np.pi * (0.2 * X[:, 0] + 0.5 * X[:, 1] - 0.6 * X[:, 2] + 0.2 * X[:, 3]))) + \
                (0.5 * (X[:, 0] + X[:, 1] + X[:, 2] + X[:, 3])) ** 3 + \
                1 / ((0.5 * X[:, 0] + 0.3 * X[:, 1] - 0.3 * X[:, 2] + 0.25 * X[:, 3]) ** 2 + 4)
     input_dim = 4
     noise_scale = 5.57
+    m_std = 1.2348424304003454
 elif is_m3:
-    def m(X):
+    def _m(X):
         return np.log(0.5 * (X[:, 0] + 0.3 * X[:, 1] + 0.6 * X[:, 2] + X[:, 3] - X[:, 4]) ** 2) + \
                np.sin(np.pi * (0.7 * X[:, 0] + X[:, 1] - 0.3 * X[:, 2] - 0.4 * X[:, 3] - 0.8 * X[:, 4])) +\
                np.cos(np.pi / (1 + np.sin(0.5 * (X[:, 1] + 0.9 * X[:, 2] - X[:, 4]))))
     input_dim = 5
     noise_scale = 6.8
+    m_std = 2.4229453384407367
 elif is_m4:
-    def m(X):
+    def _m(X):
         return np.exp(0.2 * (X[:, 0] + X[:, 1] + X[:, 2] + X[:, 3] + X[:, 4] + X[:, 5])) +\
                np.sin(np.pi / 2 * (X[:, 0] - X[:, 1] - X[:, 2] + X[:, 3] - X[:, 4] - X[:, 5])) +\
                1 / ((0.3 * X[:, 0] - 0.2 * X[:, 1] + 0.8 * X[:, 2] - 0.5 * X[:, 3] + 0.6 * X[:, 4] - 0.2 * X[:, 5]) ** 2 + 6) + \
                0.5 * (X[:, 0] + X[:, 2] - X[:, 4]) ** 3
     input_dim = 6
     noise_scale = 3.71
+    m_std = 1.8310473320070153
+
+
+if normalize_m:
+    def m(X):
+        return _m(X) / m_std
+else:
+    def m(X):
+        return _m(X)
 
 
 def load_dataset():
